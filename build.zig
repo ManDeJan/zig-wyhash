392f141b7b2f03f4fa050d2ffad783705c79f8ef
Sahnvour <sahnvour@pm.me>
2019-04-14 18:25:53 +0200

    building against the reference implementation to test results updated wyhash submodule, and tweaked a bit the implementation source file to compile in C mode

--- build.zig
+++ build.zig
@@ -7,6 +7,8 @@ pub fn build(b: *Builder) void {
 
     var tests = b.addTest("test/test.zig");
     tests.addPackagePath("wyhash", "src/main.zig");
+    tests.addCSourceFile("reference/wyhash.c", [][]const u8{});
+    tests.linkSystemLibrary("c");
     tests.setBuildMode(mode);
 
     const test_step = b.step("test", "Run library tests");
--- test/test.zig
+++ test/test.zig
@@ -3,7 +3,17 @@ const testing = std.testing;
 
 const wy = @import("wyhash");
 
+// Imported from the C object file.
+extern fn wyhash(key: *const c_void, len: u64, seed: u64) u64;
 
+test "reference test vectors" {
+    testing.expectEqual(wyhash(c"", 0, 0), 0xf961f936e29c9345);
+    testing.expectEqual(wyhash(c"a", 1, 1), 0x6dc395f88b363baa);
+    testing.expectEqual(wyhash(c"abc", 3, 2), 0x3bc9d7844798ddaa);
+    testing.expectEqual(wyhash(c"message digest", 14, 3), 0xb31238dc2c500cd3);
+    testing.expectEqual(wyhash(c"abcdefghijklmnopqrstuvwxyz", 26, 4), 0xea0f542c58cddfe4);
+    testing.expectEqual(wyhash(c"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789", 62, 5), 0x1799aca591fe73b4);
+    testing.expectEqual(wyhash(c"12345678901234567890123456789012345678901234567890123456789012345678901234567890", 80, 6), 0x7f0d02f53d64c1f9);
 }
 
 test "test vectors" {
